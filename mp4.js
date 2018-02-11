8e9d6aac8dc3a6f8f5dfe7dfd5f4239257bdd297
✔Almaftuchin <me@almaftuch.in>
2018-02-11 11:50:05 +0700

    Update mp4.js

--- mp4.js
+++ mp4.js
@@ -1,6 +1,6 @@
 $(window).load(function() {
 var linkl_list = [
-    'http://oke.io/st/?api=1b777cff0d565586e5ca50eb50e9a461073feeac&url=' + 'http://ytbapi.com/dl.php?link=[drive-id]' + '&amp;format=mp4&amp;text=fff&amp;color=ff5722'
+    'http://ytbapi.com/dl.php?link=[drive-id]' + '&amp;format=mp4&amp;text=fff&amp;color=ff5722'
   ];
   var update = function() {
     if (update_html) {
