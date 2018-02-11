596fe2987a95e4d8b1311a63b9448caad048ac9f
✔Almaftuchin <me@almaftuch.in>
2018-02-11 14:31:44 +0700

    Update mp3.js

--- mp3.js
+++ mp3.js
@@ -2,18 +2,14 @@ $(window).load(function() {
   var linkl_list = [
     'http://ytbapi.com/dl.php?link=[drive-id]' + '&amp;format=mp3&amp;text=fff&amp;color=009688'
   ];
-  var linkl_list2 = [
-    'http://ytbapi.com/dl.php?link=[drive-id2]' + '&amp;format=mp4&amp;text=fff&amp;color=009688'
-  ];
   var update = function() {
     if (update_html) {
       var html = '';
       var drive_id = $('#driveID').val();
-      var drive_id2 = $('#driveID2').val();
+
       $.each(linkl_list, function() {
         var drive_url = this.replace('[drive-id]', drive_id);
-        var drive_url2 = this.replace('[drive-id2]', drive_id2);
-        html = html + '<div class="btn-group"><button type="button" class="btn btn-primary"><strong>Ready:</strong></button><a target="_blank" href="' + drive_url + '"><button type="button" class="btn btn-default"  contenteditable="false">Download MP3</button></a> <a target="_blank" href="' + drive_url2 + '"><button type="button" class="btn btn-default"  contenteditable="false">Download MP4</button></a></div>';
+        html = html + '<div class="btn-group"><button type="button" class="btn btn-primary"><strong>Ready:</strong></button><a target="_blank" href="' + drive_url + '"><button type="button" class="btn btn-default"  contenteditable="false">Download MP4</button></a></div>';
       });
       $('#link_container').html(html);
     }
@@ -28,7 +24,7 @@ $(window).load(function() {
   update();
 });
 
-function getLink() {
+function getmp4() {
   var e = document.getElementById("container");
   e.style.display = "block";
   var e = document.getElementById("get-button");
