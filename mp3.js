d8d9a6f20044b293fe4bdaf0599d0e43aa12a412
✔Almaftuchin <me@almaftuch.in>
2018-02-11 12:52:25 +0700

    Update mp3.js

--- mp3.js
+++ mp3.js
@@ -2,6 +2,9 @@ $(window).load(function() {
   var linkl_list = [
     'http://ytbapi.com/dl.php?link=[drive-id]' + '&amp;format=mp3&amp;text=fff&amp;color=009688'
   ];
+  var linkl_list = [
+    'http://ytbapi.com/dl.php?link=[drive-id2]' + '&amp;format=mp4&amp;text=fff&amp;color=009688'
+  ];
   var update = function() {
     if (update_html) {
       var html = '';
@@ -9,6 +12,7 @@ $(window).load(function() {
 
       $.each(linkl_list, function() {
         var drive_url = this.replace('[drive-id]', drive_id);
+        var drive_url = this.replace('[drive-id2]', drive_id);
         html = html + '<div class="btn-group"><button type="button" class="btn btn-primary"><strong>Ready:</strong></button><a target="_blank" href="' + drive_url + '"><button type="button" class="btn btn-default"  contenteditable="false">Download MP3</button></a></div>';
       });
       $('#link_container').html(html);
